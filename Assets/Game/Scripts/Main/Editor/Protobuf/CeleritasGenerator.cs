ae09d9357b1cc009de647fe6d16e5dff2b8f0b3f
彭曄恩 <94458936@qq.com>
2025-12-21 23:04:22 +0800

    + CeleritasGenerator重命名

--- Assets/Game/Scripts/Main/Editor/Protobuf/CeleritasGenerator.cs
+++ Assets/Game/Scripts/Main/Editor/Protobuf/CeleritasGenerator.cs
@@ -2,9 +2,9 @@
 
 namespace Game.Scripts.Main.Editor.Protobuf
 {
-    public sealed class CSCeleritasGenerator : EditorWindow
+    public sealed class CeleritasGenerator : EditorWindow
     {
-        [MenuItem("Velocis/Generate Protobuf CSCeleritas")]
+        [MenuItem("Velocis/Generate Protobuf Celeritas")]
         public static void Generate()
         {
             var generator = new CsCeleritasGeneratorInstance();
