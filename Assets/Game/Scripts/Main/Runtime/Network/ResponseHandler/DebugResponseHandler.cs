e4bc60941457c3b5a155152a2c0e94fbe4375ce0
彭曄澤 <94458936@qq.com>
2026-01-31 10:16:22 +0800

    + 测试模块

--- Assets/Game/Scripts/Main/Runtime/Network/ResponseHandler/DebugResponseHandler.cs
+++ Assets/Game/Scripts/Main/Runtime/Network/ResponseHandler/DebugResponseHandler.cs
@@ -1,6 +1,9 @@
 ﻿using Celeritas.Proto.Client;
 using Celeritas.Proto.Common;
+using Game.Scripts.Main.Runtime.Base;
 using Game.Scripts.Main.Runtime.Network.PacketHandler;
+using Game.Scripts.Main.Runtime.UI.UICommon;
+using Game.Scripts.Main.Runtime.UI.UIMenu;
 
 namespace Game.Scripts.Main.Runtime.Network.ResponseHandler
 {
@@ -8,6 +11,12 @@ namespace Game.Scripts.Main.Runtime.Network.ResponseHandler
     {
         public override void Handle(object sender, header header, debug_response message)
         {
+            GameEntry.UI.OpenDialog(new DialogParams
+            {
+                Mode = 1,
+                Title = GameEntry.Localization.GetString("Button.Debug"),
+                Message = GameEntry.Localization.GetString("Home.OperationSuccessful")
+            });
         }
     }
 }
