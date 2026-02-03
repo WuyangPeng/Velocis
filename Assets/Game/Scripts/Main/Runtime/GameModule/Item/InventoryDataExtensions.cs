8c5e817c2d99900ea5139e495871f9c9e9c1f1b0
彭曄澤 <94458936@qq.com>
2026-02-03 14:41:22 +0800

    + 邮件

--- Assets/Game/Scripts/Main/Runtime/GameModule/Item/InventoryDataExtensions.cs
+++ Assets/Game/Scripts/Main/Runtime/GameModule/Item/InventoryDataExtensions.cs
@@ -1,22 +1,25 @@
-﻿using Celeritas.Proto.Client;
+﻿using Celeritas.Proto.Common;
 
 namespace Game.Scripts.Main.Runtime.GameModule.Item
 {
     public static class InventoryDataExtensions
     {
-        public static InventoryData ToInventoryData(this inventory_data src)
+        public static InventoryData ToInventoryData(this inventory_data data)
         {
-            if (src == null)
+            if (data == null)
             {
                 return null;
             }
 
-            var inv = new InventoryData();
-            inv.ItemId = src.ItemId;
-            inv.TemplateId = src.TemplateId;
-            inv.Count = src.Count;
-            inv.Position = src.Position;
-            return inv;
+            var inventoryData = new InventoryData
+            {
+                ItemId = data.ItemId,
+                TemplateId = data.TemplateId,
+                Count = data.Count,
+                Position = data.Position
+            };
+            
+            return inventoryData;
         }
     }
 }
