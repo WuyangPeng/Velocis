674514ae89bdcff1e77f4ea587b68a885d37e4e0
彭曄澤 <94458936@qq.com>
2026-02-04 09:53:21 +0800

    + 邮件

--- Assets/Game/Scripts/Main/Runtime/GameModule/Mail/MailModule.cs
+++ Assets/Game/Scripts/Main/Runtime/GameModule/Mail/MailModule.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using Celeritas.Proto.Client;
+using Game.Scripts.Main.Runtime.Base;
+using Game.Scripts.Main.Runtime.Network;
 using Game.Scripts.Main.Runtime.GameModule.Base;
 
 namespace Game.Scripts.Main.Runtime.GameModule.Mail
@@ -46,6 +49,59 @@ namespace Game.Scripts.Main.Runtime.GameModule.Mail
             {
                 mailData.AttachmentCollected = collected;
             }
+        }
+        
+        public void SendMailRequest(long maxMailId, int languageType)
+        {
+            var packet = ProtoHelper.GetProto();
+            var request = packet.Mutable_ClientPlayer_ClientMail_Mail();
+            request.MaxMailId = maxMailId;
+            request.LanguageType = languageType;
+            var channel = GameEntry.Network.GetNetworkChannel("TcpChannel");
+            channel.Send(packet);
+        }
+
+        public void SendMailReadRequest(long mailId)
+        {
+            var packet = ProtoHelper.GetProto();
+            var request = packet.Mutable_ClientPlayer_ClientMail_MailRead();
+            request.MailId = mailId;
+            var channel = GameEntry.Network.GetNetworkChannel("TcpChannel");
+            channel.Send(packet);
+        }
+
+        public void SendMailCollectAttachmentRequest(long mailId)
+        {
+            var packet = ProtoHelper.GetProto();
+            var request = packet.Mutable_ClientPlayer_ClientMail_MailCollectAttachment();
+            request.MailId = mailId;
+            var channel = GameEntry.Network.GetNetworkChannel("TcpChannel");
+            channel.Send(packet);
+        }
+
+        public void SendMailDeleteRequest(long mailId)
+        {
+            var packet = ProtoHelper.GetProto();
+            var request = packet.Mutable_ClientPlayer_ClientMail_MailDelete();
+            request.MailId = mailId;
+            var channel = GameEntry.Network.GetNetworkChannel("TcpChannel");
+            channel.Send(packet);
+        }
+
+        public void SendMailCollectAllAttachmentsRequest()
+        {
+            var packet = ProtoHelper.GetProto();
+            packet.Mutable_ClientPlayer_ClientMail_MailCollectAllAttachments();
+            var channel = GameEntry.Network.GetNetworkChannel("TcpChannel");
+            channel.Send(packet);
+        }
+
+        public void SendMailDeleteAllReadRequest()
+        {
+            var packet = ProtoHelper.GetProto();
+            packet.Mutable_ClientPlayer_ClientMail_MailDeleteAllRead();
+            var channel = GameEntry.Network.GetNetworkChannel("TcpChannel");
+            channel.Send(packet);
         }
     }
 }
