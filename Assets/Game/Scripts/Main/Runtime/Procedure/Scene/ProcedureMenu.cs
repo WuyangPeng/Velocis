4595f1dcc2530a319d0902a7e4e9abffa2d72723
彭曄恩 <94458936@qq.com>
2026-02-14 22:22:20 +0800

    + 清理 旧数据

--- Assets/Game/Scripts/Main/Runtime/Procedure/Scene/ProcedureMenu.cs
+++ Assets/Game/Scripts/Main/Runtime/Procedure/Scene/ProcedureMenu.cs
@@ -71,6 +71,9 @@ namespace Game.Scripts.Main.Runtime.Procedure.Scene
         private void OnServerListClose(object sender, GameEventArgs e)
         {
             RemoveUIForm(UIFormId.ServerListForm);
+
+            var accountModule = GameEntry.ModuleComponent.GetModule<AccountModule>();
+            accountModule.Clear();
         }
 
         private static void LoadAccountData()
