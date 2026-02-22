a98660281bb639932aef0e73f778cd70efa47c60
彭曄恩 <94458936@qq.com>
2026-02-22 21:46:03 +0800

    + 养成模块

--- Assets/Game/Scripts/Main/Runtime/GameModule/Develop/RoleDevelopModule.cs
+++ Assets/Game/Scripts/Main/Runtime/GameModule/Develop/RoleDevelopModule.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Game.Scripts.Main.Runtime.GameModule.Base;
 
 namespace Game.Scripts.Main.Runtime.GameModule.Develop
@@ -16,6 +17,11 @@ namespace Game.Scripts.Main.Runtime.GameModule.Develop
         public void DeleteItem(long instanceId)
         {
             Items.Remove(instanceId);
+        }
+        public int GetLevel()
+        {
+            var firstItem = Items.Values.FirstOrDefault();
+            return firstItem?.Level ?? 0;
         }
     }
 }
