43015ce35fb9ad252c697e045aa83c2b9ab65c03
彭曄澤 <94458936@qq.com>
2026-02-24 11:18:14 +0800

    + 激活道具

--- Assets/Game/Scripts/Main/Runtime/UI/UIHome/UpperForm.cs
+++ Assets/Game/Scripts/Main/Runtime/UI/UIHome/UpperForm.cs
@@ -1,7 +1,10 @@
+using Game.Scripts.Main.Runtime.GameModule.Develop;
 using Game.Scripts.Main.Runtime.GameModule.Item;
+using Game.Scripts.Main.Runtime.GameModule.Role;
 using Game.Scripts.Main.Runtime.Procedure.Scene;
 using Game.Scripts.Main.Runtime.UI.UICommon;
 using Game.Scripts.Main.Runtime.UIItem.UICreate;
+using TMPro;
 using UnityEngine;
 using UnityGameFramework.Runtime;
 using GameEntry = Game.Scripts.Main.Runtime.Base.GameEntry;
@@ -11,6 +14,10 @@ namespace Game.Scripts.Main.Runtime.UI.UIHome
     public class UpperForm : UGuiForm
     {
         [SerializeField] private AvatarItem avatarItem;
+
+        [SerializeField] private TMP_Text roleName;
+
+        [SerializeField] private TMP_Text roleLevel;
 
         private ProcedureHome _procedureHome;
 
@@ -26,29 +33,31 @@ namespace Game.Scripts.Main.Runtime.UI.UIHome
 
             var avatarModule = GameEntry.ModuleComponent.GetModule<AvatarModule>();
             var selectedAvatar = avatarModule.GetSelectedAvatar();
-            if (selectedAvatar == null)
+            if (selectedAvatar != null)
             {
-                return;
-            }
-
-            var avatarConfig = GameEntry.GameConfig.GetGameConfig().GetTables().AvatarConfigContainer.Get(selectedAvatar.Inventory.TemplateId);
-            if (avatarConfig != null)
-            {
-                avatarItem.SetSprite(avatarConfig.IconRes);
+                var avatarConfig = GameEntry.GameConfig.GetGameConfig().GetTables().AvatarConfigContainer.Get(selectedAvatar.Inventory.TemplateId);
+                if (avatarConfig != null)
+                {
+                    avatarItem.SetSprite(avatarConfig.IconRes);
+                }
             }
 
             var frameModule = GameEntry.ModuleComponent.GetModule<FrameModule>();
             var selectedFrame = frameModule.GetSelectedFrame();
-            if (selectedFrame == null)
+            if (selectedFrame != null)
             {
-                return;
+                var frameConfig = GameEntry.GameConfig.GetGameConfig().GetTables().FrameConfigContainer.Get(selectedFrame.Inventory.TemplateId);
+                if (frameConfig != null)
+                {
+                    avatarItem.SetFrameSprite(frameConfig.IconRes);
+                }
             }
 
-            var frameConfig = GameEntry.GameConfig.GetGameConfig().GetTables().FrameConfigContainer.Get(selectedFrame.Inventory.TemplateId);
-            if (frameConfig != null)
-            {
-                avatarItem.SetFrameSprite(frameConfig.IconRes);
-            }
+            var roleModule = GameEntry.ModuleComponent.GetModule<RoleModule>();
+            roleName.text = roleModule.GetFullName();
+
+            var roleDevelopModule = GameEntry.ModuleComponent.GetModule<RoleDevelopModule>();
+            roleName.text = roleDevelopModule.GetLevel() + GameEntry.Localization.GetString("Home.Level");
         }
 
         protected override void OnClose(bool isShutdown, object userData)
